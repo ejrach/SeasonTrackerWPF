14b168c2b10d8d5b19b7675a41fcb63fc06b0ca3
ejrach <eric.rach@gmail.com>
2019-10-28 13:41:15 -0500

    Created models

---
+++ SeasonTracker/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SeasonTracker.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set 
+            { 
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set 
+            { 
+                _username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set 
+            { 
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ SeasonTracker/Models/Watchlist.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SeasonTracker.Models
+{
+    public class Watchlist : INotifyPropertyChanged
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged("Id"); }
+        }
+
+        private int _userId;
+
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; OnPropertyChanged("UserId"); }
+        }
+
+        private string _tvShowName;
+
+        public string TvShowName
+        {
+            get { return _tvShowName; }
+            set { _tvShowName = value; OnPropertyChanged("TvShowName"); }
+        }
+
+        private string _season;
+
+        public string Season
+        {
+            get { return _season; }
+            set { _season = value; OnPropertyChanged("Season"); }
+        }
+
+        private string _numberOfEpisodes;
+
+        public string NumberOfEpisodes
+        {
+            get { return _numberOfEpisodes; }
+            set { _numberOfEpisodes = value; OnPropertyChanged("NumberOfEpisodes"); }
+        }
+
+        private string _episodeViewList;
+
+        public string EpisodeViewlist
+        {
+            get { return _episodeViewList; }
+            set { _episodeViewList = value; OnPropertyChanged("EpisodeViewList"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
